90abb0ee5933e91db339420131d2e253540bfd68
Sergey Eroshenkov <itechmeat@gmail.com>
2023-11-12 10:38:34 +0100

    Update fonts

--- src/features/layout/MainLayout/MainLayout.tsx
+++ src/features/layout/MainLayout/MainLayout.tsx
@@ -1,7 +1,14 @@
 import { FC, PropsWithChildren } from "react";
+import cn from "classnames";
+import { Poppins } from "next/font/google";
 import { Header } from "@/features/headline/Header/Header";
 import { Footer } from "@/features/footer/Footer/Footer";
 import styles from "./MainLayout.module.scss";
+
+const PoppinsFont = Poppins({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
 
 type MainLayoutProps = {};
 
@@ -9,7 +16,7 @@ export const MainLayout: FC<PropsWithChildren<MainLayoutProps>> = ({
   children,
 }) => {
   return (
-    <div className={styles.layout}>
+    <div className={cn(styles.layout, PoppinsFont.className)}>
       <Header className={styles.header} />
       <main className={styles.main}>
         <div className={styles.content}>{children}</div>
